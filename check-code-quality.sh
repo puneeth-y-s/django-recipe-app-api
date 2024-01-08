c737093515a7b35651a7bb4c70f413e21913a116
puneeth <yspuneeth1994@gmail.com>
2024-01-08 13:18:07 +0530

    added check code quality script

--- check-code-quality.sh
+++ check-code-quality.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+black --config .black.toml  .
+pylint --rcfile .pylintrc *.py ./recipe/*.py ./user/*.py
+flake8 --config .flake8 .
+mypy --exclude venv .
+ruff --config ruff.toml . --fix
+isort --settings .isort.cfg .
