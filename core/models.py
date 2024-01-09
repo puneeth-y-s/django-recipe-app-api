6dbb171098cd6fda6581a85e9126a2cf04a5b2fc
puneeth <yspuneeth1994@gmail.com>
2024-01-09 15:36:30 +0530

    testing merge checks

--- core/models.py
+++ core/models.py
@@ -9,6 +9,7 @@ def recipe_image_file_path(instance, filename):
     """Generate file path for new recipe image."""
     ext = os.path.splitext(filename)[1]
     filename = f"{uuid.uuid4()}{ext}"
+
     return os.path.join("uploads", "recipe", filename)
 
 
